7ceb2e11b6ea0e9f7592b0ebbfdae61ef1b6e704
Queen Danielle Garcia <queendanielleg@gmail.com>
2023-10-20 09:23:29 -0700

    spa-mpa topic

---
+++ SPA.js
@@ -0,0 +1,58 @@
+// SINGLE-PAGE APPLICATION
+// all the page/features render on a single html in web browser
+// one html is responsible to render the complete application and data is transferred using APIs (RESTFul APIs)
+// -- the page does not refresh or reload
+// -- save the data entered by user ; patch call 
+
+// reusability 
+// reduce the time of building multi-device support 
+// As APIs are specific and light weight, so it doesn't need a complex architecture like MPA
+// USER -- getUser/createUser/updateUser/deleteUser
+// PRODUCT -- getProduct/createProduct/updateProduct/deleteProduct
+
+// BACKEND -- API 
+// www.samplename.com/homeapi/gethomedata -- (fetch or post data for home page) 
+
+// FRONTEND --
+// www.samplename.com/home (bundle of home.js/home.css/home.videos)
+
+// www.samplename.com/mobilehome 
+
+
+// MULTI-PAGE APPLICATION
+// >> Entire page is build on backend and sent to browser for each page/request
+// -- when moving from one page to another; request will go to the server
+
+// www.samplename.com/home -- (java -- home.java, home.css, home.js, home(images, videos)) 
+// www.samplename.com/mobile/home (java -- mobilehome.java, mobilehome.css, mobilehome.js, mobilehome(images, videos)) 
+
+
+//  SOA -- SERVICE-ORIENTED ARCHITECTURE
+//  -- backend: Based on RESTFul APIs -- Get, Put, Post, Delete, Patch
+//  -- frontend: build using React/Angular/Backbone/Vue... (Single Page)
+
+
+
+// JAVASCRIPT
+// it is easy for developers to switch between frontend and backend
+// NodeJS and React -- both use ES6 -- advanced object oriented javascript
+
+// JavaScript -- <2015 -- before it was known as js, vanilla javascript
+// JS-2015 -- there is an upgrade to switch JS from functional programming to class-based structure -- ES6
+// ES6 == JS-2015
+// ES7 == JS-2016
+// ES8 == JS-2017
+// ES-NEXT == JS-2018+
+
+
+// JS 
+// functional
+// uses scope, hoisting, closures to provide features that we built
+
+// es6
+// multiple variables, types, classes, high-level enumerators, etc.
+
+// example: arrow function
+
+let add = (a,b) => a+b
+console.log(add(5,6))
