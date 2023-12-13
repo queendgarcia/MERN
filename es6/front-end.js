206317556663a282701e166933ccac53e65ef734
Queen Danielle Garcia <queendanielleg@gmail.com>
2023-12-12 21:59:47 -0800

    topic: webpack

---
+++ es6/front-end.js
@@ -0,0 +1,44 @@
+// Backend - Node + Express API => JSON data, JSON will be consumed on front end
+// localhost:9000/api/user/signinup => userid/{userInfo}
+
+// Express -- is a webserver that is responsible to host the application as it handles modules(?), etc. 
+// other servers: tomcat apache, etc.
+// Express the node modules to give the connections and to host the API
+// Webpack-dev-server --> webpack - core module, dev-server - is for the server(?)
+
+// Frontend -- React, lightweight-library
+// endpoints where application will be hosted 
+// localhost:9090/home
+// localhost:9090/login
+// localhost:9090/about
+// header + footer
+
+// Webpack-dev-server --> webpack - core module, dev-server - is for the server(?)
+
+// Webpack 
+// -- used as build and configuration tool ((check pptx)) as it is a module bundler not a task runner
+// -- is a tool used for development 
+
+// 1. Bundling of multiple files generated for different screens(creates a new bundle of all component.js)
+// ==> each file will be bundled into bundle.js
+// e.g. --> home.js login.js, etc. into bundle.js 
+// 2. Minification of files --> removing spaces, changing smart logical replacements, removing commented codes, redundant codes --> for performance-benefit
+
+// let stmt = "2.minification of files"
+// stmt stmt stmt stmt
+// stmt stmt stmt
+// stmt stmt stmt
+
+// 3. Minification of files can be done by js, jsx(react file), css, images, etc.
+// 4. Transpilation using Babel loaders --> converting code written in ES6/7/8/next,react into core javascript
+// browsers will not be able to understand the codes written with es6, react, etc. and will only be able to process core js, css, etc.(?)
+// creating multiple versions of application vs one version of application
+
+// 5. Support for developers to debug by having multiple versions for production support
+// 6. Monitoring, rebuilding, and hot-reloading of application with changes on the fly
+
+
+
+
+
+
