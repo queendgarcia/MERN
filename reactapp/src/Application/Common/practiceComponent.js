a6118677422e99ad878dd0ce2c5a847ad434d421
Queen Danielle Garcia <queendanielleg@gmail.com>
2023-12-14 09:07:33 -0800

    practice: create own component

---
+++ reactapp/src/Application/Common/practiceComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+let PracticeComponent = () => {
+  return (
+    <>
+      <h2>This is from practice component</h2>
+      <h4>Queen Garcia</h4>
+      <h4>USA</h4>
+      <h4>MERNStack Session: Personal Component</h4>
+    </>
+  )
+}
+
+export default PracticeComponent;
--- reactapp/src/Application/app.js
+++ reactapp/src/Application/app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HeaderComponent from "./Common/headerComponent";
+import PracticeComponent from "./Common/practiceComponent";
 
 let ApplicationComponent = () => {
   let a = 10, b = 5;
@@ -9,6 +10,7 @@ let ApplicationComponent = () => {
       {/* perform operation inside curly braces */}
       <b>The Arithmetic Operation {a+b} {a*b} {a-b} {a/b}</b>
       <h1>This is the Application Component</h1>
+      <PracticeComponent />
     </>
   )
 }
