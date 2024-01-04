39097fa18ef7aefc2dc64fd3c4f03fb72e246513
Queen Danielle Garcia <queendanielleg@gmail.com>
2024-01-04 00:24:20 -0800

    topic: props

--- reactapp/src/Application/Common/headerComponent.js
+++ reactapp/src/Application/Common/headerComponent.js
@@ -4,6 +4,8 @@ let HeaderComponent = (props) => {
   return (
     <>
       <h2>Shopping Cart Application</h2>
+      <h3>{props.header}</h3>
+      <h3>Name - {props.name}</h3>
     </>
   )
 }
--- reactapp/src/Application/app.js
+++ reactapp/src/Application/app.js
@@ -1,18 +1,38 @@
-import React from "react";
-import HeaderComponent from "./Common/headerComponent";
-import PracticeComponent from "./Common/practiceComponent";
+import React, {Component} from "react";
+import HeaderComponent from "./Common/HeaderComponent";
+import PracticeComponent from "./Common/PracticeComponent";
 
-let ApplicationComponent = () => {
-  let a = 10, b = 5;
-  return (
-    <>
-      <HeaderComponent />
-      {/* perform operation inside curly braces */}
-      <b>The Arithmetic Operation {a+b} {a*b} {a-b} {a/b}</b>
-      <h1>This is the Application Component</h1>
-      <PracticeComponent />
-    </>
-  )
+// class component
+export default class ApplicationComponent extends Component {
+
+  render() {
+    let a = 10, b = 5;
+    let header = "10,001+ employees · UI Specialist"
+    return (
+      <>
+        <HeaderComponent header={header} name={"Name 1"}/>
+        {/* perform operation inside curly braces */}
+        <b>The Arithmetic Operation {a+b} {a*b} {a-b} {a/b}</b>
+        <h1>This is the Application Component</h1>
+        <h1>{header}</h1>
+        {/* <PracticeComponent /> */}
+      </>
+    )
+  }
 }
 
-export default ApplicationComponent;
+//functional component
+// let ApplicationComponent = ()=>{
+//     let a = 10, b = 5;
+//     let header = "10,001+ employees · UI Specialist"
+//     return(
+//         <>
+//             <HeaderComponent/>
+//             <b>The Arithmatic Operation {a+b} {a*b} {a-b} {a/b}</b>
+//             <h1>This is the Application Component</h1>
+//             <h1>{header}</h1>
+//         </>
+//     )
+// }
+
+// export default ApplicationComponent
